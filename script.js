6b6a7624c2c5f68632a308f96537a86c2cb58067
Dhanushika Jeganadhan <113341523+DhanushikaJeganadhan@users.noreply.github.com>
2022-10-16 21:52:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+(function(){
+    let screen= document.querySelector('.screen');
+    let button=document.querySelectorAll('.Btn');
+    let clear=document.querySelector('.green');
+    let equal=document.querySelector('.red');
+    
+    button.forEach(function(buttons){
+        buttons.addEventListener('click',function(event){
+            let value=event.target.dataset.num;
+            screen.value+=value;
+        })
+    })
+    //dsf
+    
 
+
+})();
